35a525cf950a37e5bff540a8733ba0feb9246c62
v1ssar1on <evartanyan@anykey.pro>
2025-10-22 12:24:17 +0300

    feat: init project

---
+++ frontend-2/vite.config.ts
@@ -0,0 +1,38 @@
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
+
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+
+    return {
+        plugins: [react(), tailwindcss()],
+        define: {
+            global: 'window',
+        },
+        server: {
+            open: true,
+            port: 5173,
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                    secure: true,
+                },
+            },
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+                '@pages': path.resolve(__dirname, './src/pages'),
+                '@widgets': path.resolve(__dirname, './src/widgets'),
+                '@features': path.resolve(__dirname, './src/features'),
+                '@entities': path.resolve(__dirname, './src/entities'),
+                '@shared': path.resolve(__dirname, './src/shared'),
+            },
+        },
+    }
+})
